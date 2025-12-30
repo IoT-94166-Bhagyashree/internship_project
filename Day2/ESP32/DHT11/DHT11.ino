eb544777aaa6020ae50ba068b156946cfb272abb
Bhagyashree <bhagyashrijadhav017@gmail.com>
2025-12-30 16:58:22 +0530

    Day 2: ESP32 setup and DHT11 & MQ-2 sensor interfacing

---
+++ Day2/ESP32/DHT11/DHT11.ino
@@ -0,0 +1,25 @@
+#include "DHT.h"
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print(" °C  ");
+
+  Serial.print("Humidity: ");
+  Serial.println(hum);
+
+  delay(2000);
+}
---
+++ Day2/docs/System Architecture.md
@@ -0,0 +1,3 @@
+## System Architecture
+ESP32 reads sensor data → sends data to ThingSpeak cloud.
+BeagleBone Black handles MQTT-based processing and alerts.
